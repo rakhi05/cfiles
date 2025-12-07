868542224910f5607e56b45f0a344305b41052e5
rakhi05 <rakhi662006@gmail.com>
2025-12-07 22:20:37 +0530

    update

---
+++ circlearea.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ #include<conio.h>
+ int main()
+ {float rad,pi,area;
+ clrscr();
+ printf("enter rad");
+ scanf("%f",&rad);
+ pi=3.14;
+ area=pi*rad*rad;
+ printf("%f is the area of circle",area);
+ getch();
+ return 0;
+ }
