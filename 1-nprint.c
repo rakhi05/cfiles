83cb507e8beb9ce1883467acf0f1044c39500cb6
rakhi05 <rakhi662006@gmail.com>
2025-12-09 09:53:57 +0530

    update

---
+++ 1-nprint.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int limit,i;
+    i=1;
+    scanf("%d",& limit);
+    while(i<=limit)
+    {
+        printf("%d",i);
+        i++;
+    }
+    return 0;
+}
